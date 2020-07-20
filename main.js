5705ade993be5239c258ec26cb7a74036cf657ed
Thomas Price <thomas.price@acresoftware.com>
2020-07-20 02:35:16 +0100

    Add regex

--- main.js
+++ main.js
@@ -45,10 +45,8 @@ const listItemDropdownString = (item) => `
 </div>`;
 
 const isValidURL = (str) => {
-  const url = new RegExp(
-    "https?://(www.)?[-a-zA-Z0-9@:%._+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)"
-  );
-
+  const exp = /[https?://(www.)?][-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  const url = new RegExp(exp);
   if (url.test(str)) {
     return true;
   }
